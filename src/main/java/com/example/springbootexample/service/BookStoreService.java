6c8fe03412416a5e32e70f99514a44f3201b3288
Sharmila <sharmilapriya94@gmail.com>
2021-08-11 12:24:17 +0530

    SpringBoot with Restful web services first commit;

---
+++ src/main/java/com/example/springbootexample/service/BookStoreService.java
@@ -0,0 +1,57 @@
+package com.example.springbootexample.service;
+
+import com.example.springbootexample.entity.BookDetails;
+import com.example.springbootexample.exception.BookNotFoundException;
+import com.example.springbootexample.repository.BookStoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Sharmila
+ */
+@Service
+@Transactional
+public class BookStoreService {
+
+    @Autowired
+    BookStoreRepository bookStoreRepository;
+
+    public BookDetails addBookDetails(BookDetails bookDetails){
+        return bookStoreRepository.save(bookDetails);
+    }
+
+    public List<BookDetails> getBookDetails(){
+        return bookStoreRepository.findAll();
+    }
+
+    public BookDetails getBookDetail(Integer bookId){
+
+        return bookStoreRepository.findById(bookId)
+                .orElseThrow(()->new BookNotFoundException("Book not found for this id :: " + bookId));
+    }
+
+    public BookDetails updateBookDetails(BookDetails bookDetails){
+        BookDetails bookDetails1 = new BookDetails();
+        bookDetails1.setBookId(bookDetails.getBookId());
+        bookDetails1.setBookName(bookDetails.getBookName());
+        bookDetails1.setBookCategory(bookDetails.getBookCategory());
+        bookDetails1.setPublisher(bookDetails.getPublisher());
+        bookDetails1.setAuthor(bookDetails.getAuthor());
+        bookDetails1.setBookPublishedDate(bookDetails.getBookPublishedDate());
+        return bookStoreRepository.save(bookDetails1);
+    }
+
+    public void deleteBookDetails(Integer bookId) {
+        BookDetails bookDetails = bookStoreRepository.findById(bookId)
+                .orElseThrow(()->new BookNotFoundException("Book not found for this id :: " + bookId));
+        if(bookDetails!=null) {
+            bookStoreRepository.deleteById(bookId);
+        }
+    }
+
+}
